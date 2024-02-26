622378c5c262697cea6349544b69b1af838e2af6
manmohan5072 <161231811+manmohan5072@users.noreply.github.com>
2024-02-26 15:01:14 +0530

    Add files via upload

---
+++ Pages/Components/nav.js
@@ -0,0 +1,44 @@
+var footer=document.createElement("nav")
+footer.innerHTML =`<nav>
+<div class="navbar_custom">
+	
+<div class="./content/images/LOGO.png">
+    <h2>last bench</h2>
+</div>
+
+<ul class="links">
+    <li><a href="https://www.instagram.com/thakur_manmohan_singh_5072">Developer</a></li>
+</ul>
+
+
+<div class="toggle">
+    <div class="line1"></div>
+    <div class="line2"></div>
+    <div class="line3"></div>
+</div>
+</div>
+</nav>
+    `
+document.body.appendChild(footer)
+
+
+
+// nav toogle 
+const navbar = document.querySelector('.navbar_custom')
+
+navbar.querySelector('.toggle').addEventListener('click',()=>{
+	
+	navbar.classList.toggle('collapsed')
+	
+})
+
+window.addEventListener('scroll',(e)=>{
+	
+	let windowY = window.pageYOffset
+	
+	let navbarHeight = document.querySelector('.navbar').offsetHeight
+	
+	if(windowY>navbarHeight) navbar.classList.add('sticky')
+	else navbar.classList.remove('sticky')
+	
+})
